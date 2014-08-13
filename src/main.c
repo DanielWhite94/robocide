4b4cb2653814412a67ee587c37e9dadb5fd4e026
DanielWhite94 <DanielWhite94@server.fake>
2014-08-13 14:05:52 +0100

    Added mainFatalError() function to main.c to provide a way for the engine to abort.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "attacks.h"
 #include "bb.h"
 #include "eval.h"
+#include "main.h"
 #include "pos.h"
 #include "search.h"
 #include "uci.h"
@@ -19,4 +22,13 @@ int main()
   EvalQuit();
   
   return 0;
+}
+
+void mainFatalError(const char *Format, ...)
+{
+  va_list ap;
+  va_start(ap, Format);
+  vfprintf(stderr, Format, ap);
+  va_end(ap);
+  exit(EXIT_FAILURE);
 }
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+void mainFatalError(const char *Format, ...);
+
+#endif

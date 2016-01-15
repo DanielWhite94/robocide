DanielWhite94/robocide

commit 42cecf9a66fbaa2a3acfa323e7883386293688a2

searchNodeNext is now calculated to be at most 1s in the future.

src/search.c
@@ -129,7 +129,7 @@ void searchThink(const Pos *srcPos, const SearchLimit *limit) {
 	if (pos==NULL)
 		return;
 	searchNodeCount=0;
-	searchNodeNext=~0llu; // So we never check clock unless time is specified (see below).
+	searchNodeNext=0;
 	searchStopFlag=false;
 	while (lockTryWait(searchActivity)) ; // Reset to 0.
 	searchEndTime=TimeMsInvalid;
@@ -716,7 +716,7 @@ bool searchIsTimeUp(void) {
 			// already used 12s, we aim to check again at 14s (12+(16-12)/2).
 			// We use the node counter and previous nps as a rough timer to avoid
 			// checking the real time too often.
-			TimeMs timeDelay=64*(searchEndTime-currTime); // We /128 later to avoid losing accuracy.
+			TimeMs timeDelay=64*utilMin(searchEndTime-currTime, 1000); // We /128 later to avoid losing accuracy. Also limit to 1s.
 			unsigned long long int nodeDelay=(searchNodeCount*timeDelay)/(128*(currTime-searchLimit.startTime));
 			searchNodeNext=searchNodeCount+nodeDelay;
 		} else

commit abc91c0323fa771ec8ae3792aff5eb534c09c3b4

Fixed subtle bug introduced in commit 42cecf9a66fbaa2a3acfa323e7883386293688a2. Setting searchNodeNext=0 made the following line in searchIsTimeUp() have no effect: searchNodeNext*=2.

src/search.c
@@ -133,7 +133,7 @@ void searchThink(const Pos *srcPos, const SearchLimit *limit) {
 	if (pos==NULL)
 		return;
 	searchNodeCount=0;
-	searchNodeNext=0;
+	searchNodeNext=1;
 	searchShowCurrmove=false;
 	searchStopFlag=false;
 	while (lockTryWait(searchActivity)) ; // Reset to 0.
@@ -157,10 +157,8 @@ void searchThink(const Pos *srcPos, const SearchLimit *limit) {
 	}
 	if (searchLimit.moveTime!=TimeMsInvalid)
 		searchTime=utilMin(searchTime, searchLimit.moveTime);
-	if (searchTime!=TimeMsInvalid) {
+	if (searchTime!=TimeMsInvalid)
 		searchEndTime=searchLimit.startTime+searchTime;
-		searchNodeNext=1;
-	}
 
 	// Set away worker
 	threadRun(searchThread, &searchIDLoop, (void *)pos);
@@ -744,7 +742,7 @@ bool searchIsTimeUp(void) {
 			// already used 12s, we aim to check again at 14s (12+(16-12)/2).
 			// We use the node counter and previous nps as a rough timer to avoid
 			// checking the real time too often.
-			TimeMs timeDelay=64*utilMin(searchEndTime-currTime, 1000); // We /128 later to avoid losing accuracy. Also limit to 1s.
+			TimeMs timeDelay=64*utilMin(searchEndTime-currTime, 2*1000); // We /128 later to avoid losing accuracy. Also limit to 1s.
 			unsigned long long int nodeDelay=(searchNodeCount*timeDelay)/(128*(currTime-searchLimit.startTime));
 			searchNodeNext=searchNodeCount+nodeDelay;
 		} else
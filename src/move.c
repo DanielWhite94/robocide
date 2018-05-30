cf9a8bfc3da0bd92382e4fd2c0272381fedd9046
DJW <dan@fake.com>
2018-05-30 02:19:29 +0100

    Updated moveIsDP.

--- src/move.c
+++ src/move.c
@@ -69,7 +69,7 @@ Colour moveGetColour(Move move) {
 
 bool moveIsDP(Move move) {
 	assert(moveIsValid(move));
-	int toRank=sqRank(moveGetToSq(move));
+	int toRank=sqRank(moveGetToSqRaw(move));
 	int fromRank=sqRank(moveGetFromSq(move));
 	return (moveGetToPieceType(move)==PieceTypePawn && abs(toRank-fromRank)==2);
 }

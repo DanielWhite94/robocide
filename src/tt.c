ee0e7586b7c43f504e7415e3c3702c34c435af49
DJW <dan@fake.com>
2018-06-07 20:32:22 +0100

    TT now implements its own hash table to reduce overheads.

--- src/tt.c
+++ src/tt.c
@@ -1,10 +1,11 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "htable.h"
 #include "main.h"
 #include "search.h"
+#include "thread.h"
 #include "tt.h"
 #include "uci.h"
 #include "util.h"
@@ -16,28 +17,36 @@ STATICASSERT(DepthBit<=8);
 STATICASSERT(BoundBit<=2);
 STATICASSERT(DateBit<=6);
 typedef struct {
-	uint16_t keyUpper;
-	uint16_t move;
-	int16_t score;
-	uint8_t depth;
-	uint8_t bound:2;
-	uint8_t date:6; // Search date at the time the entry was last read/written, used to calculate entry age.
+	union {
+		uint64_t int64;
+		struct {
+			uint16_t keyUpper;
+			uint16_t move;
+			int16_t score;
+			uint8_t depth;
+			uint8_t bound:2;
+			uint8_t date:6; // Search date at the time the entry was last read/written, used to calculate entry age.
+		};
+	};
 } TTEntry;
 
 // Group ttClusterSize number of entries into each 'bin'.
 // When reading from the tt we loop over all entries in the relevant bin, when
 // writing we choose the 'least useful' entry to replace.
-#define ttClusterSize (4u)
-typedef struct {
-	TTEntry entries[ttClusterSize];
-} TTCluster;
 
-HTable *tt=NULL;
+unsigned ttEntryCount=0;
+AtomicUInt64 *tt=NULL;
+
+typedef uint32_t TTKey;
 
 const size_t ttDefaultSizeMb=16;
-#define ttMaxClusters HTableMaxEntryCount // 2^32
-#define ttMaxEntries (ttMaxClusters*ttClusterSize) // 2^34
-const size_t ttMaxSizeMb=(ttMaxClusters*sizeof(TTCluster))/(1024*1024); // 128gb
+#define ttEntriesPerCluster 4
+#define ttEntrySize 8
+STATICASSERT(ttEntrySize==sizeof(AtomicUInt64));
+STATICASSERT(ttEntrySize==sizeof(TTEntry));
+#define ttKeySize 32 // Only lower 32 bits are used, limiting maximum number of entries
+#define ttMaxEntries ((1llu)<<(ttKeySize)) // 2^32
+const size_t ttMaxSizeMb=(ttMaxEntries*sizeof(AtomicUInt64))/(1024*1024); // 32gb
 
 ////////////////////////////////////////////////////////////////////////////////
 // Private prototypes.
@@ -51,49 +60,52 @@ unsigned int ttEntryFitness(unsigned int age, Depth depth, bool exact);
 Score ttScoreOut(Score score, Depth ply);
 Score ttScoreIn(Score score, Depth ply);
 
-HTableKey ttHTableKeyFromPos(const Pos *pos);
+TTKey ttKeyFromPos(const Pos *pos);
+
+bool ttResize(unsigned sizeMb); // sizeMb>0
+void ttResizeInterface(void *userData, long long int value); // Interface for UCI spin option code.
+
+void ttClearInterface(void *userData); // Interface for UCI button option code.
 
 ////////////////////////////////////////////////////////////////////////////////
 // Public functions.
 ////////////////////////////////////////////////////////////////////////////////
 
 void ttInit(void) {
-	// Setup tt as a HTable.
-	tt=htableNew(sizeof(TTCluster), ttDefaultSizeMb, 0);
-	if (tt==NULL)
+	// Attempt to allocate at default size.
+	if (!ttResize(ttDefaultSizeMb))
 		mainFatalError("Error: Could not allocate transposition table.\n");
 
 	// Add uci options to change size and clear.
-	uciOptionNewSpin("Hash", &htableResizeInterface, tt, 1, ttMaxSizeMb, ttDefaultSizeMb);
-	uciOptionNewButton("Clear Hash", &htableClearInterface, tt);
+	uciOptionNewSpin("Hash", &ttResizeInterface, NULL, 1, ttMaxSizeMb, ttDefaultSizeMb);
+	uciOptionNewButton("Clear Hash", &ttClearInterface, NULL);
 }
 
 void ttQuit(void) {
-	htableFree(tt);
+	free((void *)tt);
 	tt=NULL;
+	ttEntryCount=0;
 }
 
 void ttClear(void) {
-	htableClear(tt);
+	memset((void *)tt, 0, ttEntryCount*ttEntrySize);
 }
 
 bool ttRead(const Pos *pos, Depth ply, Move *move, Depth *depth, Score *score, Bound *bound) {
-	// Grab cluster.
-	HTableKey hTableKey=ttHTableKeyFromPos(pos);
-	volatile TTCluster *cluster=htableGrab(tt, hTableKey);
-	htableRelease(tt, hTableKey); // There is no locking on the tt so we might as well call this now.
+	unsigned index=((((uint64_t)ttKeyFromPos(pos))*((uint64_t)ttEntryCount))>>32);
+	index&=~((unsigned)ttEntriesPerCluster);
 
 	// Loop over entries in cluster looking for a match.
 	unsigned int i;
-	for(i=0;i<ttClusterSize;++i) {
-		TTEntry entry=cluster->entries[i];
+	for(i=0;i<ttEntriesPerCluster;++i,++index) {
+		TTEntry entry={.int64=atomicUInt64Get(tt+index)};
 
 		if (!ttEntryMatch(pos, entry))
 			continue;
 
 		// Update entry date (to reset age to 0).
 		entry.date=searchGetDate();
-		cluster->entries[i]=entry;
+		atomicUInt64Set(tt+index, entry.int64);
 
 		// Extract information.
 		*move=entry.move;
@@ -125,18 +137,14 @@ void ttWrite(const Pos *pos, Depth ply, Depth depth, Move move, Score score, Bou
 	assert(scoreIsValid(score));
 	assert(bound!=BoundNone);
 
-	uint64_t key=posGetKey(pos);
-
-	// Grab cluster.
-	HTableKey hTableKey=ttHTableKeyFromPos(pos);
-	volatile TTCluster *cluster=htableGrab(tt, hTableKey);
-	htableRelease(tt, hTableKey); // See comment in ttRead call to this.
+	unsigned index=((((uint64_t)ttKeyFromPos(pos))*((uint64_t)ttEntryCount))>>32);
+	index&=~((unsigned)ttEntriesPerCluster);
 
 	// Find entry to overwrite.
-	unsigned int replaceIndex=0;
+	unsigned replaceIndex=index;
 	unsigned int i, replaceScore=0; // Worst possible score.
-	for(i=0;i<ttClusterSize;++i) {
-		TTEntry entry=cluster->entries[i];
+	for(i=0;i<ttEntriesPerCluster;++i,++index) {
+		TTEntry entry={.int64=atomicUInt64Get(tt+index)};
 
 		// If we find an exact match, simply reuse this entry.
 		// We can also be certain that if this entry is unused, we will not find an
@@ -144,7 +152,7 @@ void ttWrite(const Pos *pos, Depth ply, Depth depth, Move move, Score score, Bou
 		// instead been written to this unused entry).
 		if (ttEntryMatch(pos, entry) || ttEntryUnused(entry)) {
 			// Set key (in case entry was previously unused).
-			entry.keyUpper=(key>>48);
+			entry.keyUpper=(posGetKey(pos)>>48);
 
 			// Update entry date (to reset age to 0).
 			entry.date=searchGetDate();
@@ -161,7 +169,7 @@ void ttWrite(const Pos *pos, Depth ply, Depth depth, Move move, Score score, Bou
 			}
 
 			// Update cluster/table
-			cluster->entries[i]=entry;
+			atomicUInt64Set(tt+index, entry.int64);
 
 			return;
 		}
@@ -169,36 +177,30 @@ void ttWrite(const Pos *pos, Depth ply, Depth depth, Move move, Score score, Bou
 		// Otherwise check if entry is better to use than replace.
 		unsigned int entryScore=ttEntryFitness(searchDateToAge(entry.date), entry.depth, entry.bound==BoundExact);
 		if (entryScore>replaceScore) {
-			replaceIndex=i;
+			replaceIndex=index;
 			replaceScore=entryScore;
 		}
 	}
 
 	// Replace entry.
 	TTEntry replaceEntry;
-	replaceEntry.keyUpper=(key>>48);
+	replaceEntry.keyUpper=(posGetKey(pos)>>48);
 	replaceEntry.move=move;
 	replaceEntry.score=ttScoreIn(score, ply);
 	replaceEntry.depth=depth;
 	replaceEntry.bound=bound;
 	replaceEntry.date=searchGetDate();
 
-	cluster->entries[replaceIndex]=replaceEntry;
+	atomicUInt64Set(tt+replaceIndex, replaceEntry.int64);
 }
 
 unsigned int ttFull(void) {
 	unsigned total=0;
 
-	unsigned checked=0;
-	size_t index, indexDelta=ttMaxClusters/1000;
-	for(index=0;checked<1000;index+=indexDelta) {
-		TTCluster *cluster=htableGrab(tt, index);
-
-		unsigned entry;
-		for(entry=0; entry<ttClusterSize && checked<1000; ++entry,++checked)
-			total+=(!ttEntryUnused(cluster->entries[entry]));
-
-		htableRelease(tt, index);
+	unsigned index, indexDelta=ttEntryCount/1000, checked;
+	for(index=0,checked=0;checked<1000;index+=indexDelta,++checked) {
+		TTEntry entry={.int64=atomicUInt64Get(tt+index)};
+		total+=!ttEntryUnused(entry);
 	}
 
 	return total;
@@ -245,9 +247,48 @@ Score ttScoreIn(Score score, Depth ply) {
 		return score;
 }
 
-HTableKey ttHTableKeyFromPos(const Pos *pos) {
+TTKey ttKeyFromPos(const Pos *pos) {
 	assert(pos!=NULL);
 
-	STATICASSERT(HTableKeySize==32);
+	STATICASSERT(ttKeySize==32);
 	return posGetKey(pos)&0xFFFFFFFFu; // Use lower 32 bits
+}
+
+bool ttResize(unsigned sizeMb) {
+	// Limit size due to key size.
+	if (sizeMb>ttMaxSizeMb)
+		sizeMb=ttMaxSizeMb;
+
+	// Find number of entries we can fit in the given size (rounded down to a multiple of the cluster size).
+	unsigned entryCount=(1024llu*1024llu*sizeMb)/ttEntrySize;
+	entryCount=(entryCount/ttEntriesPerCluster)*ttEntriesPerCluster;
+	while(entryCount>0) {
+		// Attempt to allocate this number of entries.
+		AtomicUInt64 *tempPtr=realloc((void *)tt, entryCount*ttEntrySize);
+		if (tempPtr!=NULL) {
+			tt=tempPtr;
+			ttEntryCount=entryCount;
+
+			ttClear();
+
+			return true;
+		}
+
+		// Try again with fewer entries.
+		entryCount/=2;
+	}
+
+	return false;
+}
+
+void ttResizeInterface(void *userData, long long int value) {
+	assert(userData!=NULL);
+
+	ttResize(value);
+}
+
+void ttClearInterface(void *userData) {
+	assert(userData!=NULL);
+
+	ttClear();
 }

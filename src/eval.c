51cbec95df9d2172981c3ed5b07e53abd01cbbe7
DJW <fake@example.com>
2025-03-29 11:30:59 +0000

    Removed space from clear pawn/mat hash UCI option names in case a GUI gets confused

--- src/eval.c
+++ src/eval.c
@@ -246,14 +246,14 @@ void evalInit(void) {
 	if (evalPawnTable==NULL)
 		mainFatalError("Error: Could not allocate pawn hash table.\n");
 	uciOptionNewSpin("PawnHash", &htableResizeInterface, evalPawnTable, 1, evalPawnTableMaxSizeMb, evalPawnTableDefaultSizeMb);
-	uciOptionNewButton("Clear PawnHash", &htableClearInterface, evalPawnTable);
+	uciOptionNewButton("ClearPawnHash", &htableClearInterface, evalPawnTable);
 
 	// Setup mat hash table.
 	evalMatTable=htableNew(sizeof(EvalMatData), evalMatTableDefaultSizeMb);
 	if (evalMatTable==NULL)
 		mainFatalError("Error: Could not allocate mat hash table.\n");
 	uciOptionNewSpin("MatHash", &htableResizeInterface, evalMatTable, 1, evalMatTableMaxSizeMb, evalMatTableDefaultSizeMb);
-	uciOptionNewButton("Clear MatHash", &htableClearInterface, evalMatTable);
+	uciOptionNewButton("ClearMatHash", &htableClearInterface, evalMatTable);
 
 	// Calculate dervied values (such as passed pawn table).
 	evalRecalc();

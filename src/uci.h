7e179cabfe447b3e37c4e2af9ca4713bfa7041d1
DJW <fake@example.com>
2025-04-21 16:15:56 +0100

    Fixed argument order in uciOptionNewSpin prototype.

--- src/uci.h
+++ src/uci.h
@@ -11,7 +11,7 @@ void uciLoop(void);
 bool uciWrite(const char *format, ...);
 
 bool uciOptionNewCheck(const char *name, void(*function)(void *userData, bool value), void *userData, bool initial);
-bool uciOptionNewSpin(const char *name, void(*function)(void *userData, long long int value), void *userData, long long int initial, long long int min, long long int max);
+bool uciOptionNewSpin(const char *name, void(*function)(void *userData, long long int value), void *userData, long long int min, long long int max, long long int initial);
 bool uciOptionNewCombo(const char *name, void(*function)(void *userData, const char *value), void *userData, const char *initial, size_t optionCount, ...);
 bool uciOptionNewButton(const char *name, void(*function)(void *userData), void *userData);
 bool uciOptionNewString(const char *name, void(*function)(void *userData, const char *value), void *userData, const char *initial);

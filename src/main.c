5487f6ddbbdd9b439685bf3bc592c9d46255cd9b
DJW <fake@example.com>
2025-06-01 19:44:53 +0100

    Updated replay logic mainReplay to handle new log file format (without using hash and history table files).

--- src/main.c
+++ src/main.c
@@ -175,94 +175,141 @@ void mainLogSearchEnd(unsigned long long int nodeCount) {
 }
 
 bool mainReplay(const char *path, const char *date) {
-	// Reset state
-	searchClear();
-	evalClear();
-
-	// Import TT and history data
-	char ttPath[4096]; // TODO: better
-	sprintf(ttPath, "%s.TT.%s", path, date);
-	if (!ttImport(ttPath)) {
-		printf("Error: could not import TT data at '%s'\n", ttPath);
-		return false;
-	}
-
-	char historyPath[4096]; // TODO: better
-	sprintf(historyPath, "%s.Hist.%s", path, date);
-	if (!historyImport(historyPath)) {
-		printf("Error: could not import history data at '%s'\n", historyPath);
-		return false;
-	}
-
-	// Read position and nodes searched
+	// Open log file
 	FILE *file=fopen(path, "r");
 	if (file==NULL) {
 		printf("Error: could not read log data at '%s'\n", path);
 		return false;
 	}
 
-	char posStr[4096]={0}; // TODO: better
-	unsigned long long int nodes=0;
-	TimeMs searchTimeMs=0;
+	// Read log file looking for the given date string (and also for preceeding ucinewgame location)
+	long ucinewgamePos=-1;
+	long datePos=-1;
 
-	printf("Log file data:\n");
-	bool inMatchingEntry=false;
 	char line[4096]; // TODO: better
-	while(fgets(line, 4096, file)!=NULL) {
+	while(1) {
+		// Read line (taking note of current position before we do so)
+		long linePos=ftell(file);
+		if (fgets(line, 4096, file)==NULL)
+			break;
+
+		// Strip newline to make parsing simpler
 		if (line[0]!='\0' && line[strlen(line)-1]=='\n')
 			line[strlen(line)-1]='\0';
 
-		if (strncmp(line, "date ", 5)==0) {
-			if (strcmp(line+5, date)==0)
-				inMatchingEntry=true;
-			else if (inMatchingEntry)
-				break; // end of entry - no need to read further into the file
-		} else if (inMatchingEntry && strncmp(line, "position ", 9)==0) {
-			strcpy(posStr, line);
-		} else if (inMatchingEntry && strncmp(line, "nodes ", 6)==0) {
-			nodes=atoll(line+6);
-		} else if (inMatchingEntry && strncmp(line, "searchtime ", 11)==0) {
-			searchTimeMs=atoll(line+11);
-		} else if (inMatchingEntry && strncmp(line, "info ", 5)==0) {
-			printf("%s\n", line+5);
+		// Parse line
+		if (strcmp(line, "ucinewgame")==0) {
+			ucinewgamePos=linePos;
+		} else if (strncmp(line, "date ", 5)==0 && strcmp(line+5, date)==0) {
+			datePos=linePos;
+			break;
 		}
 	}
 
+	if (ucinewgamePos==-1 || datePos==-1) {
+		printf("Error: could not find given date string/preceeding ucinewgame\n");
+		fclose(file);
+		return false;
+	}
+
+	// Start replaying game from first move (or first move we had to search for after any book opening)
+	// First clear internal search state.
+	searchClear();
+	evalClear();
+
+	// Now start replaying game, move by move, until desired position reached.
+	if (fseek(file, ucinewgamePos, SEEK_SET)!=0) {
+		printf("Error: seek error (ucinewgame)\n");
+		fclose(file);
+		return false;
+	}
+
+	bool inEntry=false;
+	char posStr[4096]; // TODO: better
+	unsigned long long int nodes;
+	while(1) {
+		// Read line (taking note of current position before we do so)
+		long linePos=ftell(file);
+		if (fgets(line, 4096, file)==NULL)
+			break;
+
+		// Strip newline to make parsing simpler
+		if (line[0]!='\0' && line[strlen(line)-1]=='\n')
+			line[strlen(line)-1]='\0';
+
+		// Parse line
+		if (inEntry) {
+			if (strncmp(line, "date ", 5)==0) {
+				// Should have encountered 'nodes' before this
+				printf("Error: file consistency error (subsequent 'date' before terminating 'nodes')\n");
+				fclose(file);
+				return false;
+			} else if (strncmp(line, "position ", 9)==0) {
+				// Grab position and also output it
+				strcpy(posStr, line);
+				printf("\n%s\n", posStr);
+				printf("Log data:\n");
+			} else if (strncmp(line, "info ", 5)==0) {
+				// Output info lines from log file
+				printf("info %s\n", line+5);
+			} else if (strncmp(line, "nodes ", 6)==0) {
+				// Grab nodes and also output it
+				nodes=atoll(line+6);
+				printf("info nodes %llu\n", nodes);
+
+				// Check we have all the info we need from the log file
+				inEntry=false;
+				if (posStr[0]=='\0') {
+					printf("Error: could not read position string for log file entry\n");
+					fclose(file);
+					return false;
+				}
+
+				if (nodes==0) {
+					printf("Error: could not read node count for log file entry\n");
+					fclose(file);
+					return false;
+				}
+
+				// Set position instance
+				Pos *pos=posNew(NULL);
+				if (!uciPosFromStr(pos, posStr)) {
+					printf("Error: could set position from string '%s'\n", posStr);
+					posFree(pos);
+					fclose(file);
+					return false;
+				}
+
+				// Search
+				SearchLimit limit;
+				searchLimitInit(&limit, timeGet());
+				searchLimitSetNodes(&limit, nodes);
+
+				printf("Replay data:\n");
+				searchThink(pos, &limit, SearchOutputPartial);
+				searchWait();
+
+				// Tidy up
+				posFree(pos);
+			}
+		} else {
+			if (strncmp(line, "date ", 5)==0) {
+				// Have we reached the end of the entry the caller specified?
+				if (linePos>datePos)
+					break;
+
+				// Reset for new position/search instance/entry
+				posStr[0]='\0';
+				nodes=0;
+				inEntry=true;
+			}
+		}
+	}
+
+	printf("\nReplay complete\n");
+
+	// Close log files
 	fclose(file);
-
-	if (posStr[0]=='\0') {
-		printf("Error: could not read position string from log file\n");
-		return false;
-	}
-
-	if (nodes==0) {
-		printf("Error: could not read node count from log file\n");
-		return false;
-	}
-
-	if (searchTimeMs==0) {
-		printf("Error: could not read search time from log file\n");
-		return false;
-	}
-
-	// Set position instance
-	Pos *pos=posNew(NULL);
-	if (!uciPosFromStr(pos, posStr)) {
-		posFree(pos);
-		printf("Error: could set position from string '%s'\n", posStr);
-		return false;
-	}
-
-	// Search
-	SearchLimit limit;
-	searchLimitInit(&limit, timeGet());
-	searchLimitSetNodes(&limit, nodes);
-
-	printf("Replay data:\n");
-	searchThink(pos, &limit, SearchOutputPartial);
-
-	// Tidy up
-	posFree(pos);
 
 	return true;
 }

f923a7e920b65ebeecdfa25db27233013dd93609
Daniel White <fake@example.com>
2026-08-09 21:25:53 +0100

    Improved net.c early file structure and added useful includes

--- src/net.h
+++ src/net.h
@@ -1,12 +1,21 @@
 #ifndef NET_H
 #define NET_H
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "colour.h"
+#include "piece.h"
 #include "pos.h"
 #include "score.h"
+#include "square.h"
 
+// General functions
 void netInit(void);
 void netQuit(void);
 
+// Evaluation functions
 Score netEvaluateRaw(const Pos *pos);
 
 #endif

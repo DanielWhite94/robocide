c30e516f3a57ea5c0a08e8ef69fac1873b4dc1de
Daniel White <fake@example.com>
2026-08-14 19:18:37 +0100

    Readded tuning code we removed in a previous commit while restructuring

---
+++ src/nnue/tune.c
@@ -0,0 +1,223 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../pos.h"
+#include "../util.h"
+#include "tune.h"
+
+typedef enum {
+	NnueTuneScoreInvalid,
+	NnueTuneScoreWhiteWin,
+	NnueTuneScoreBlackWin,
+	NnueTuneScoreDraw,
+} NnueTuneScore;
+
+const char NnueTuneScoreStr[][8]={
+	[NnueTuneScoreInvalid]="???",
+	[NnueTuneScoreWhiteWin]="1-0",
+	[NnueTuneScoreBlackWin]="0-1",
+	[NnueTuneScoreDraw]="1/2-1/2",
+};
+
+STATICASSERT(PieceNB<=16);
+typedef struct {
+	BB occ;
+	uint8_t pieces[16]; // a pair per byte in order from a1-h8
+	uint8_t stm:1;
+	uint8_t score:2;
+	uint8_t padding:5;
+} NnueTunePosition;
+
+struct NnueTunePositions {
+	NnueTunePosition *array;
+	size_t next, size;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Private prototypes
+////////////////////////////////////////////////////////////////////////////////
+
+void nnueTunePositionSetFromPos(NnueTunePosition *position, const Pos *pos);
+Piece nnueTunePositionGetPiece(const NnueTunePosition *pos, unsigned n);
+void nnueTunePositionDebug(const NnueTunePosition *pos);
+
+const char *nnueTuneScoreToStr(NnueTuneScore score);
+
+////////////////////////////////////////////////////////////////////////////////
+// Public functions
+////////////////////////////////////////////////////////////////////////////////
+
+NnueTunePositions *nnueTunePositionsNew(size_t count) {
+	// Allocate memory
+	NnueTunePositions *positions=malloc(sizeof(NnueTunePositions));
+	NnueTunePosition *array=(count>0 ? malloc(sizeof(NnueTunePosition)*count) : NULL);
+	if (positions==NULL || (count>0 && array==NULL))
+		return NULL;
+
+	// Set fields
+	positions->array=array;
+	positions->next=0;
+	positions->size=count;
+
+	return positions;
+}
+
+void nnueTunePositionsFree(NnueTunePositions *positions) {
+	// NULL check
+	if (positions==NULL)
+		return;
+
+	// Free memory
+	free(positions->array);
+	free(positions);
+}
+
+NnueTunePositions *nnueTunePositionsLoadEpd(const char *path) {
+	// Init variables
+	NnueTunePositions *positions=NULL;
+	FILE *file=NULL;
+	Pos *pos=NULL;
+
+	// Open EPD file for reading
+	file=fopen(path, "r");
+	if (file==NULL)
+		goto error;
+
+	// Allocate NnueTunePositions object
+	unsigned lineCount=utilFileCountLines(file); // do an initial pass over the file to count the number of lines so we can allocate our positions object all at once
+	positions=nnueTunePositionsNew(lineCount);
+	if (positions==NULL)
+		goto error;
+
+	// Create scratch position and coefficients to use to find evaluation coefficients
+	pos=posNew(NULL);
+
+	// Read file one line at a time
+	char line[1024];
+	while(fgets(line, 1024, file)!=NULL) {
+		// Try to parse FEN string and game result
+		char *c9pos=strstr(line, " c9 \"");
+		if (c9pos==NULL)
+			continue;
+		*c9pos='\0';
+
+		char *fenStr=line;
+		char *resultStr=c9pos+5;
+		char *resultStrEndPos=strstr(resultStr, "\"");
+		if (resultStrEndPos==NULL)
+			continue;
+		*resultStrEndPos='\0';
+
+		NnueTuneScore result=NnueTuneScoreInvalid;
+		if (strcmp(resultStr, "1-0")==0)
+			result=NnueTuneScoreWhiteWin;
+		else if (strcmp(resultStr, "1/2-1/2")==0)
+			result=NnueTuneScoreDraw;
+		else if (strcmp(resultStr, "0-1")==0)
+			result=NnueTuneScoreBlackWin;
+		else
+			continue;
+
+		// Setup position
+		if (!posSetToFEN(pos, fenStr))
+			continue;
+
+		// Add position to array
+		assert(positions->next<positions->size);
+
+		nnueTunePositionSetFromPos(&positions->array[positions->next], pos);
+		positions->array[positions->next].score=result;
+
+		++positions->next;
+	}
+
+	// Tidy up
+	posFree(pos);
+	fclose(file);
+
+	return positions;
+
+	// Error handling
+	error:
+	posFree(pos);
+	if (file!=NULL)
+		fclose(file);
+	nnueTunePositionsFree(positions);
+	return NULL;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Private functions
+////////////////////////////////////////////////////////////////////////////////
+
+void nnueTunePositionSetFromPos(NnueTunePosition *position, const Pos *pos) {
+	assert(position!=NULL);
+	assert(pos!=NULL);
+
+	// Occ is a direct copy
+	BB occ=position->occ=posGetBBAll(pos);
+
+	// Create piece list
+	memset(position->pieces, 0, sizeof(position->pieces));
+	unsigned i=0;
+	while(occ!=BBNone) {
+		Sq sq=bbScanReset(&occ);
+		Piece p=posGetPieceOnSq(pos, sq);
+		assert(p!=PieceNone && p<PieceNB && PieceNB<=16);
+		position->pieces[i/2]|=(i%2==0 ? p : (p<<4));
+		++i;
+	}
+
+	// Misc fields
+	position->stm=posGetSTM(pos);
+	position->score=NnueTuneScoreInvalid; // this will be set after by the caller
+}
+
+Piece nnueTunePositionGetPiece(const NnueTunePosition *pos, unsigned n) {
+	assert(pos!=NULL);
+	assert(n<32);
+
+	return ((pos->pieces[n/2]>>(n%2==0 ? 0 : 4)) & 0xF);
+}
+
+void nnueTunePositionDebug(const NnueTunePosition *pos) {
+	// Bitscan loop to find pieces
+	Piece array[SqNB];
+	assert(PieceNone==0);
+	memset(array, 0, sizeof(array));
+
+	unsigned i=0;
+	BB occ=pos->occ;
+	while(occ!=BBNone) {
+		Sq sq=bbScanReset(&occ);
+		Piece p=nnueTunePositionGetPiece(pos, i);
+		assert(p!=PieceNone && p<PieceNB);
+
+		array[sq]=p;
+
+		++i;
+	}
+
+	// Print board and other fields
+	int file, rank;
+	for(rank=Rank8;rank>=Rank1;--rank) {
+		printf("%c|", rankToChar(rank));
+		for(file=FileA;file<=FileH;++file)
+			printf(" %c", pieceToChar(array[sqMake(file,rank)]));
+		printf("\n");
+	}
+	printf("   ----------------\n");
+	printf("  ");
+	for(file=FileA;file<=FileH;++file)
+		printf(" %c", fileToChar(file));
+	printf("\n");
+	printf("STM = %s\n", colourToStr(pos->stm));
+	printf("Result = %s\n", nnueTuneScoreToStr(pos->score));
+}
+
+const char *nnueTuneScoreToStr(NnueTuneScore score) {
+	assert(score<4);
+	return NnueTuneScoreStr[score];
+}
---
+++ src/nnue/tune.h
@@ -0,0 +1,11 @@
+#ifndef NNUE_TUNE_H
+#define NNUE_TUNE_H
+
+typedef struct NnueTunePositions NnueTunePositions; // a collection of positions each with the final game result
+
+NnueTunePositions *nnueTunePositionsNew(size_t count); // count can be 0 if not known in advance
+void nnueTunePositionsFree(NnueTunePositions *positions);
+
+NnueTunePositions *nnueTunePositionsLoadEpd(const char *path);
+
+#endif
